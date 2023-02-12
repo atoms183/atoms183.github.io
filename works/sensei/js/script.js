e08ef4ac17978f694387dd836ae56d4c9d17811c
Дмитрий Глазвин <sony183@bk.ru>
2023-02-12 19:49:45 +0300

    Add files via upload

---
+++ works/sensei/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+$('.header__burger').click(function(event) {
+$('.header__burger,.header__menu').toggleClass('active');
+$('body').toggleClass('lock');
+});
+});
+ 
+
+ const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
