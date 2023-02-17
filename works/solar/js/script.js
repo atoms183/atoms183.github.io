dc0fe2c8f7489f511f913f65f529fbb59433ff0f
Дмитрий Глазвин <sony183@bk.ru>
2023-02-17 20:23:32 +0300

    Add files via upload

--- works/solar/js/script.js
+++ works/solar/js/script.js
@@ -1,9 +1,16 @@
-
+   /* (function ($) {
+      $(window).on("load", function () {
+        $('body').mCustomScrollbar({
+          theme: "dark"
+        });
+      });
+    })(jQuery);*/
+    
     $(document).ready(function () {
       $('.slider').slick({
         arrows:true,
         slidesToShow: 1,
-        autoplay:true,
+        autoplay:false,
          speed:3000,
          autoplaySpeed:800,
            responsive:[
@@ -16,3 +23,34 @@
          ]
       });
      });
+
+     
+// ========== Переменные для "tabo'v" ==========
+const tabsTitle = document.querySelectorAll('.column-tab__title');
+const tabsContent = document.querySelectorAll('.tab__content');
+
+
+// Перебираем все заголовки табов и вешаем на них click
+tabsTitle.forEach(item => item.addEventListener('click', event => {
+    
+    // Получаем атрибут заголовка на который мы кликнули конкретно!
+    const tabsTitleTarget = event.target.getAttribute('data-tab');
+
+    // Удаляем от всех заголовков класс active-tab
+    tabsTitle.forEach(element => element.classList.remove('active-tab'));
+
+    // Добавляем всем tab__content класс hidden-tab-content, который скрывает содержимое!
+    tabsContent.forEach(element => element.classList.add('hidden-tab-content'));
+
+    // Добавляем класс active-tab, заголовку который нажали в данный момент!
+    item.classList.add('active-tab');
+
+    // Удаляем класс hidden-tab-content, чтобы показывалось его содержимое!
+    document.getElementById(tabsTitleTarget).classList.remove('hidden-tab-content');
+
+}));
+
+// Мы указываем, какая конктретно вкладка должна быть активна при загрузке страницы со старта,
+// если нужно, чтобы все вкладки ыли скрыты изначально, то удалите код ниже!
+document.querySelector('[data-tab="tab-1"]').classList.add('active-tab');
+document.querySelector('#tab-1').classList.remove('hidden-tab-content');
