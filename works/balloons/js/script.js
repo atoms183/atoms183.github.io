0bc01181f8d4dd31e8b7bf040f9f44b6486a71a1
Дмитрий Глазвин <sony183@bk.ru>
2023-02-22 14:36:46 +0300

    Add files via upload

--- works/balloons/js/script.js
+++ works/balloons/js/script.js
@@ -5,3 +5,28 @@ $('body').toggleClass('lock');
 });
 });
  
+$(document).ready(function () {
+    $('.slider').slick({
+      arrows:true,
+      slidesToShow: 4,
+      autoplay:true,
+       speed:1800,
+       autoplaySpeed:800,
+         responsive:[
+         {
+           breakpoint: 1250,
+           settings: {
+             slidesToShow:3
+           },
+           breakpoint: 992,
+           settings: {
+             slidesToShow:2
+           },
+           breakpoint: 670,
+           settings: {
+             slidesToShow:2
+           }
+         }
+       ]
+    });
+   });
