793273d98e5c30ffa34d01e480a0fe903c8eff86
Дмитрий Глазвин <sony183@bk.ru>
2023-03-13 11:10:17 +0300

    Add files via upload

---
+++ works/school/js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+
+
+$('.header__burger').click(function(event) {
+$('.header__burger,.header__menu').toggleClass('active');
+$('body').toggleClass('lock');
+});
+
+$('.popup-ceremony__link').magnificPopup({
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+$('.popup-with-form').magnificPopup({
+  type: 'inline',
+  preloader: false,
+  focus: '#name',
+
+  // When elemened is focused, some mobile browsers in some cases zoom in
+  // It looks not nice, so we disable it:
+  callbacks: {
+    beforeOpen: function() {
+      if($(window).width() < 700) {
+        this.st.focus = false;
+      } else {
+        this.st.focus = '#name';
+      }
+    }
+  }
+});
+
+
+});
+ 
