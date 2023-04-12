f88f8a59ca06f70734d9279701172b8e932f5c2c
Дмитрий Глазвин <sony183@bk.ru>
2023-04-12 19:42:02 +0300

    Add files via upload

---
+++ works/levelland/js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+$('.header__burger').click(function(event) {
+$('.header__burger,.header__menu').toggleClass('active');
+$('body').toggleClass('lock');
+});
+$('.gallery__image').lightGallery({
+});
+$('.content-howchoose__title').click(function(event) {
+  if($('.howchoose__content').hasClass('one')){
+      $('.content-howchoose__title').not($(this)).removeClass('active');
+      $('.content-howchoose__text').not($(this).next()).slideUp(300);
+  }
+  $(this).toggleClass('active').next().slideToggle(300);
+});
+});
+ 
+ 
+
+var swiper = new Swiper(".mySwiper", {
+   autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },  
+    effect: "flip",
+    grabCursor: true,
+  });
