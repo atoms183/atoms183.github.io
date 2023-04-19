7f412ade01de5ef5ff705f9acab0a710b30a96fe
Дмитрий Глазвин <sony183@bk.ru>
2023-04-19 19:43:23 +0300

    Add files via upload

--- works/lightblog/js/script.js
+++ works/lightblog/js/script.js
@@ -4,4 +4,13 @@ $('.header__burger,.header__menu').toggleClass('active');
 $('body').toggleClass('lock');
 });
 });
- 
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
