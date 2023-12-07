f43dd243a91dbe6989cb1e2ccdcf4455e5c6eb89
Дмитрий Глазвин <sony183@bk.ru>
2023-12-07 06:13:21 +0300

    Add files via upload

---
+++ works/ITResult/js/script.js
@@ -0,0 +1,39 @@
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu');
+if (iconMenu){
+iconMenu.addEventListener("click", function(e){
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+});
+}
+
+
+//Прокрутка при клике
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLinks => {
+        menuLinks.addEventListener("click", onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            if(iconMenu.classList.contains('_active')) {
+                document.body.classList.remove('lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
