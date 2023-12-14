d4e3bcc3bb14aa4e93dadd03ba090595c7a9683c
Дмитрий Глазвин <sony183@bk.ru>
2023-12-14 20:51:40 +0300

    Add files via upload

--- works/kadastr/js/script.js
+++ works/kadastr/js/script.js
@@ -109,3 +109,90 @@ var swiper = new Swiper(".cover__swiper", {
      },
   });
  
+//   var elem = document.getElementsByClassName('about-us');
+//   var sourceBottom = elem[0].getBoundingClientRect().bottom + window.pageYOffset;
+//   window.onscroll = function() {
+//       if (window.pageYOffset < sourceBottom) {
+//         window.onload = function() {
+  
+//             var options1 = {
+//               useEasing : true, 
+//               useGrouping : true
+//             };
+//             var item1 = new CountUp("number1", 0, 200, 0, 2.5, options1);
+//             item1.start();
+//             var item2 = new CountUp("number2", 0, 1500, 0, 2.5, options1);
+//             item2.start();
+//             var item3 = new CountUp("number3", 0, 99, 0, 2.5, options1);
+//             item3.start();
+//             var item4 = new CountUp("number4", 0, 11, 0, 2.5, options1);
+//             item4.start();
+//           }
+        
+//       }
+//   };
+
+
+// var number = document.querySelector('.number'),
+// numberTop = number.getBoundingClientRect().top,
+// start = +number.innerHTML, end = +number.dataset.max;
+
+// window.addEventListener('scroll', function onScroll() {
+//     if(window.pageYOffset > numberTop - window.innerHeight / 2) {
+//         this.removeEventListener('scroll', onScroll);
+//         var interval = setInterval(function() {
+//             number.innerHTML = ++start;
+//             if(start == end) {
+//                 clearInterval(interval);
+//             }
+//         }, 5);
+//     }
+// });
+
+// var number1 = document.querySelector('.number1'),
+// number1Top = number1.getBoundingClientRect().top,
+// start1 = +number1.innerHTML, end1 = +number1.dataset.max;
+
+// window.addEventListener('scroll', function onScroll() {
+//     if(window.pageYOffset > number1Top - window.innerHeight / 2) {
+//         this.removeEventListener('scroll', onScroll);
+//         var interval1 = setInterval(function() {
+//             number1.innerHTML = ++start1;
+//             if(start1 == end1) {
+//                 clearInterval(interval1);
+//             }
+//         }, 5);
+//     }
+// });
+
+// var number2 = document.querySelector('.number2'),
+// 		number2Top = number2.getBoundingClientRect().top,
+// 		start2 = +number2.innerHTML, end2 = +number2.dataset.max;
+
+// 		window.addEventListener('scroll', function onScroll() {
+// 			if(window.pageYOffset > number2Top - window.innerHeight / 2) {
+// 				this.removeEventListener('scroll', onScroll);
+// 				var interval2 = setInterval(function() {
+// 					number2.innerHTML = ++start2;
+// 					if(start2 == end2) {
+// 						clearInterval(interval2);
+// 					}
+// 				}, 5);
+// 			}
+// 		});
+
+//         var number3 = document.querySelector('.number3'),
+// 		number3Top = number3.getBoundingClientRect().top,
+// 		start3 = +number3.innerHTML, end3 = +number3.dataset.max;
+
+// 		window.addEventListener('scroll', function onScroll() {
+// 			if(window.pageYOffset > number3Top - window.innerHeight / 2) {
+// 				this.removeEventListener('scroll', onScroll);
+// 				var interval3 = setInterval(function() {
+// 					number3.innerHTML = ++start3;
+// 					if(start3 == end3) {
+// 						clearInterval(interval3);
+// 					}
+// 				}, 5);
+// 			}
+// 		});
