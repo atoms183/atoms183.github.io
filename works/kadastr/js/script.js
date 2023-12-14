565daebea829b80a51231cab746a32f0f814086d
Дмитрий Глазвин <sony183@bk.ru>
2023-12-14 15:01:02 +0300

    Add files via upload

--- works/kadastr/js/script.js
+++ works/kadastr/js/script.js
@@ -52,3 +52,40 @@ if (iconMenu){
         menuBody.classList.toggle('_active');
     });
 }
+
+var swiper = new Swiper(".cover__swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+
+  var swiper = new Swiper(".services__swiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev",
+     },
+     breakpoints: {
+       480: {
+         slidesPerView: 2,
+         spaceBetween: 27,
+       },
+       768: {
+         slidesPerView: 2,
+         spaceBetween: 27,
+       },
+       1024: {
+         slidesPerView: 4,
+         spaceBetween: 27,
+       }
+     },
+  });
