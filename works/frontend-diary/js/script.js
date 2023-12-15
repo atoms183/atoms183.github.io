a6b0e9699eab05374e1ce609174026c012241e9f
Дмитрий Глазвин <sony183@bk.ru>
2023-12-15 17:02:23 +0300

    Add files via upload

---
+++ works/frontend-diary/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+$('.header__burger').click(function(event) {
+$('.header__burger,.header__menu').toggleClass('active');
+$('body').toggleClass('lock');
+});
+});
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
+  hljs.highlightAll();
