9aa538fd502ff8d256e4b713a43f9b20e1d70541
Дмитрий Глазвин <sony183@bk.ru>
2023-12-18 15:57:11 +0300

    Add files via upload

--- works/kadastr/js/script.js
+++ works/kadastr/js/script.js
@@ -127,6 +127,30 @@ if (iconMenu){
   });
 
 
+  const tabNavItems = document.querySelectorAll('.tabs-useful__button');
+  const tabItems = document.querySelectorAll('.content-tabs__item-inner');
+  document.addEventListener("click", function (e) {
+      const targetElement = e.target;
+      let currentActiveIndex = null;
+      let newActiveIndex = null;
+      if (targetElement.closest('.tabs-useful__button')) {
+          tabNavItems.forEach((tabNavItem, index) => {
+              if (tabNavItem.classList.contains('active')) {
+                  currentActiveIndex = index;
+                  tabNavItem.classList.remove('active');
+              }
+              if (tabNavItem === targetElement) {
+                  newActiveIndex = index;
+              }
+          });
+          targetElement.classList.add('active');
+          tabItems[currentActiveIndex].classList.remove('active');
+          tabItems[newActiveIndex].classList.add('active');
+      }
+  });
+
+
+
  
 //   var elem = document.getElementsByClassName('about-us');
 //   var sourceBottom = elem[0].getBoundingClientRect().bottom + window.pageYOffset;
