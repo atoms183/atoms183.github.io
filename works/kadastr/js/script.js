30d046183efc7e1b65fb2f2028cce5ab3452cc68
Дмитрий Глазвин <sony183@bk.ru>
2023-12-19 12:29:23 +0300

    Add files via upload

--- works/kadastr/js/script.js
+++ works/kadastr/js/script.js
@@ -1,61 +1,4 @@
-"use strict"
-
-    const isMobile = {
-        Android: function () {
-            return navigator.userAgent.match(/Android/i);
-        },
-        BlackBerry: function () {
-            return navigator.userAgent.match(/BlackBerry/i);
-        },
-        iOS: function () {
-            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-        },
-        Opera: function () {
-            return navigator.userAgent.match(/Opera Mini/i);
-        },
-        Windows: function () {
-            return navigator.userAgent.match(/IEMobile/i);
-        },
-        any: function () {
-            return (
-                isMobile.Android() ||
-                isMobile.BlackBerry() ||
-                isMobile.iOS() ||
-                isMobile.Opera() ||
-                isMobile.Windows());
-        }
-    };
-    if (isMobile.any()) {
-        document.body.classList.add('_touch');
-    
-    let menuArrows = document.querySelectorAll('.menu__arrow');
-        if (menuArrows.length > 0) {
-            for (let index = 0; index < menuArrows.length; index++) {
-                const menuArrow = menuArrows[index];
-               menuArrow.addEventListener("click", function(e) {
-                    menuArrow.parentElement.classList.toggle('_active');
-               });
-            }
-        }
-    
-    } else {
-        document.body.classList.add('_pc');
-    }
-    
-    
-    const iconMenu = document.querySelector('.menu__icon');
-    const menuBody = document.querySelector('.menu');
-if (iconMenu){
-    iconMenu.addEventListener("click", function(e){
-        document.body.classList.toggle('lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
-}
-
-
-
-  var swiper = new Swiper(".projects__swiper, .another-projects__swiper, .what-say__swiper", {
+var swiper = new Swiper(".projects__swiper, .another-projects__swiper, .what-say__swiper", {
     slidesPerView: 1,
     spaceBetween: 20,
     pagination: {
@@ -81,6 +24,7 @@ if (iconMenu){
        }
      },
   });
+
 
   var swiper = new Swiper(".our-clients__swiper, .services__swiper, .documentation__swiper", {
     slidesPerView: 1,
@@ -151,53 +95,63 @@ if (iconMenu){
 
 
 
+
+"use strict"
+
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (
+                isMobile.Android() ||
+                isMobile.BlackBerry() ||
+                isMobile.iOS() ||
+                isMobile.Opera() ||
+                isMobile.Windows());
+        }
+    };
+    if (isMobile.any()) {
+        document.body.classList.add('_touch');
+    
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+        if (menuArrows.length > 0) {
+            for (let index = 0; index < menuArrows.length; index++) {
+                const menuArrow = menuArrows[index];
+               menuArrow.addEventListener("click", function(e) {
+                    menuArrow.parentElement.classList.toggle('_active');
+               });
+            }
+        }
+    
+    } else {
+        document.body.classList.add('_pc');
+    }
  
+    const iconMenu = document.querySelector('.menu__icon');
+    const menuBody = document.querySelector('.menu');
+if (iconMenu){
+    iconMenu.addEventListener("click", function(e){
+        document.body.classList.toggle('lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
 
 
-// var number = document.querySelector('.number'),
-// numberTop = number.getBoundingClientRect().top,
-// start = +number.innerHTML, end = +number.dataset.max;
 
-// window.addEventListener('scroll', function onScroll() {
-//     if(window.pageYOffset > numberTop - window.innerHeight / 2) {
-//         this.removeEventListener('scroll', onScroll);
-//         var interval = setInterval(function() {
-//             number.innerHTML = ++start;
-//             if(start == end) {
-//                 clearInterval(interval);
-//             }
-//         }, 5);
-//     }
-// });
 
-// var number1 = document.querySelector('.number1'),
-// number1Top = number1.getBoundingClientRect().top,
-// start1 = +number1.innerHTML, end1 = +number1.dataset.max;
 
-// window.addEventListener('scroll', function onScroll() {
-//     if(window.pageYOffset > number1Top - window.innerHeight / 2) {
-//         this.removeEventListener('scroll', onScroll);
-//         var interval1 = setInterval(function() {
-//             number1.innerHTML = ++start1;
-//             if(start1 == end1) {
-//                 clearInterval(interval1);
-//             }
-//         }, 5);
-//     }
-// });
 
-// var number2 = document.querySelector('.number2'),
-// 		number2Top = number2.getBoundingClientRect().top,
-// 		start2 = +number2.innerHTML, end2 = +number2.dataset.max;
-
-// 		window.addEventListener('scroll', function onScroll() {
-// 			if(window.pageYOffset > number2Top - window.innerHeight / 2) {
-// 				this.removeEventListener('scroll', onScroll);
-// 				var interval2 = setInterval(function() {
-// 					number2.innerHTML = ++start2;
-// 					if(start2 == end2) {
-// 						clearInterval(interval2);
-// 					}
-// 				}, 5);
-// 			}
-// 		});
